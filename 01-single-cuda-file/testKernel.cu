639055c237c53d1fdd395f5acf02ffcb99196721
nccvector <fzn1020@gmail.com>
2024-01-27 01:31:25 +0900

    function param fix

--- 01-single-cuda-file/testKernel.cu
+++ 01-single-cuda-file/testKernel.cu
@@ -5,7 +5,7 @@
 
 // KERNEL
 __global__ void testKernel(float *out, float *a, float *b, int n){
-  for(int i=0; i<N; i++)
+  for(int i=0; i<n; i++)
   {
     out[i] = a[i] + b[i];
   }
@@ -17,22 +17,21 @@ void kernelCaller(float *out, float *a, float *b, int n){
   float *da, *db, *dout;
 
   // Dedicate memory on device
-  cudaMalloc((void **) &da, sizeof(float) * N);
-  cudaMalloc((void **) &db, sizeof(float) * N);
-  cudaMalloc((void **) &dout, sizeof(float) * N);
-
+  cudaMalloc((void **) &da, sizeof(float) * n);
+  cudaMalloc((void **) &db, sizeof(float) * n);
+  cudaMalloc((void **) &dout, sizeof(float) * n);
   // Copy host to device
-  cudaMemcpy(da, a, sizeof(float) * N, cudaMemcpyHostToDevice);
-  cudaMemcpy(db, b, sizeof(float) * N, cudaMemcpyHostToDevice);
-  cudaMemcpy(dout, out, sizeof(float) * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(da, a, sizeof(float) * n, cudaMemcpyHostToDevice);
+  cudaMemcpy(db, b, sizeof(float) * n, cudaMemcpyHostToDevice);
+  cudaMemcpy(dout, out, sizeof(float) * n, cudaMemcpyHostToDevice);
 
   // Execute kernel
-  testKernel<<<1, 1>>>(dout, da, db, N);
+  testKernel<<<1, 1>>>(dout, da, db, n);
 
   // Copy device to host
-  cudaMemcpy(a, da, sizeof(float) * N, cudaMemcpyDeviceToHost);
-  cudaMemcpy(b, db, sizeof(float) * N, cudaMemcpyDeviceToHost);
-  cudaMemcpy(out, dout, sizeof(float) * N, cudaMemcpyDeviceToHost);
+  cudaMemcpy(a, da, sizeof(float) * n, cudaMemcpyDeviceToHost);
+  cudaMemcpy(b, db, sizeof(float) * n, cudaMemcpyDeviceToHost);
+  cudaMemcpy(out, dout, sizeof(float) * n, cudaMemcpyDeviceToHost);
 }
 
 int main() {
