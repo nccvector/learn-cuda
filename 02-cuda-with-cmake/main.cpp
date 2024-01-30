48a2d69b7436d804dcf6e5114f6a3b582c8ca9da
nccvector <fzn1020@gmail.com>
2024-01-30 23:16:01 +0900

    cmake example more generic (managing memories in cpp file)

--- 02-cuda-with-cmake/main.cpp
+++ 02-cuda-with-cmake/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <cuda_runtime.h>
+
 #include "testkernel.h"
 
 #define N 1000
+
 
 int main() {
   // Create some memory in host
@@ -17,10 +20,30 @@ int main() {
     out[i] = 0;
   }
 
-  kernelCaller(out.data(), a.data(), b.data(), N);
+  //================================================================================
+  // Create device vars
+  float *da, *db, *dout;
 
-  for (int i=0; i<N; i++)
-  {
+  // Dedicate memory on device
+  cudaMalloc((void **) &da, sizeof(float) * N);
+  cudaMalloc((void **) &db, sizeof(float) * N);
+  cudaMalloc((void **) &dout, sizeof(float) * N);
+
+  // Copy host to device
+  cudaMemcpy(da, a.data(), sizeof(float) * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(db, b.data(), sizeof(float) * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(dout, out.data(), sizeof(float) * N, cudaMemcpyHostToDevice);
+
+  // Execute kernel
+  wrapperKernel(dout, da, db, N);
+
+  // Copy device to host
+  cudaMemcpy(a.data(), da, sizeof(float) * N, cudaMemcpyDeviceToHost);
+  cudaMemcpy(b.data(), db, sizeof(float) * N, cudaMemcpyDeviceToHost);
+  cudaMemcpy(out.data(), dout, sizeof(float) * N, cudaMemcpyDeviceToHost);
+  //================================================================================
+
+  for (int i = 0; i < N; i++) {
     std::cout << out[i] << std::endl;
   }
 }
--- 02-cuda-with-cmake/testkernel.cu
+++ 02-cuda-with-cmake/testkernel.cu
@@ -1,33 +1,12 @@
 #include "testkernel.h"
 
 // KERNEL
-__global__ void testKernel(float *out, float *a, float *b, int n){
-  for(int i=0; i<n; i++)
-  {
+__global__ void testKernel(float *out, float *a, float *b, int n) {
+  for (int i = 0; i < n; i++) {
     out[i] = a[i] + b[i];
   }
 }
 
-// CALLER
-void kernelCaller(float *out, float *a, float *b, int n){
-
-  float *da, *db, *dout;
-
-  // Dedicate memory on device
-  cudaMalloc((void **) &da, sizeof(float) * n);
-  cudaMalloc((void **) &db, sizeof(float) * n);
-  cudaMalloc((void **) &dout, sizeof(float) * n);
-
-  // Copy host to device
-  cudaMemcpy(da, a, sizeof(float) * n, cudaMemcpyHostToDevice);
-  cudaMemcpy(db, b, sizeof(float) * n, cudaMemcpyHostToDevice);
-  cudaMemcpy(dout, out, sizeof(float) * n, cudaMemcpyHostToDevice);
-
-  // Execute kernel
-  testKernel<<<1, 1>>>(dout, da, db, n);
-
-  // Copy device to host
-  cudaMemcpy(a, da, sizeof(float) * n, cudaMemcpyDeviceToHost);
-  cudaMemcpy(b, db, sizeof(float) * n, cudaMemcpyDeviceToHost);
-  cudaMemcpy(out, dout, sizeof(float) * n, cudaMemcpyDeviceToHost);
+void wrapperKernel(float *out, float *a, float *b, int n) {
+  testKernel<<<1, 1>>>(out, a, b, n);
 }
